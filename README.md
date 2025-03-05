f7d8c4f4713d73faee63d5d01f86b924e846cdc6
Vidar Hoel <vidar.hoel@sysrq.info>
2025-03-05 15:03:01 +0100

    Inital commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# helloredis
+
+A simple Flask webpage using redis to track hostname, first and last visit, and number of visits. The idea is to run one Redis-server and multiple webapps in the Kubernetes cluster.
+This part also contains a `Dockerfile` and `docker-compose.yml` to create the container and verify it using Docker.
+
+The Docker images built are hosted on my public repo at Docker Hub: https://hub.docker.com/r/sitronno/redishello
+
+#### Notes
+
+- I use the `bullseye` and `buster` version of python3-slim and redis Docker images, since my Kubernetes lab consists of Raspberry Pi's, running Raspbian 10.
+- To build and upload multi-arch images to Docker Hub, use [buildx](https://github.com/docker/buildx): `docker buildx build --push --platform linux/arm/v7,linux/amd64 --tag sitronno/redishello:v1.0 .`
---
+++ app.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template, url_for
+from redis import Redis, RedisError
+import os
+import socket
+import datetime
+
+# Connect to Redis
+redis = Redis(host=os.getenv("REDIS_SERVER", "localhost"), db=0, decode_responses=True, socket_connect_timeout=2, socket_timeout=2)
+
+app = Flask(__name__)
+
+def genTable(redis, hostname):
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    if redis.exists(hostname):
+        redis.hset(hostname, "last_seen", now)
+        redis.hincrby(hostname, "counter", 1)
+    else:
+        redis.hset(hostname, "first_seen", now)
+        redis.hset(hostname, "last_seen", now)
+        redis.hset(hostname, "counter", 1)
+
+    servers = []
+    for key in redis.scan_iter():
+        temp_dict = redis.hgetall(key)
+        temp_dict['hostname'] = key
+        servers.append(temp_dict)
+
+    return servers
+
+@app.route("/")
+def hello():
+
+    redis_html = ""
+    try:
+        redis_html += "Connection to Redis (servername: {}): {}<br />".format(os.getenv("REDIS_SERVER", "localhost"), redis.ping())
+        server_data = genTable(redis, socket.gethostname())
+    except RedisError:
+        redis_html += "Cannot update or insert data into Redis"
+        return render_template('error.html', hostname=socket.gethostname(), redis_html=redis_html)
+
+    return render_template('redis.html', hostname=socket.gethostname(), server_list=server_data, redis_server=os.getenv("REDIS_SERVER", "localhost"))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
